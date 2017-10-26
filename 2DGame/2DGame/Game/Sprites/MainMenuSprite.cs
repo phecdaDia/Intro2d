a46bd0261ded1bdf4f8d209bea840b972b5e3c50
Phecda <imthe666st@gmail.com>
2017-10-26 20:50:10 +0200

    A bit more Menu work.

--- 2DGame/2DGame/Game/Sprites/MainMenuSprite.cs
+++ 2DGame/2DGame/Game/Sprites/MainMenuSprite.cs
@@ -13,13 +13,15 @@ namespace Intro2DGame.Game.Sprites
     {
         // Dictionary used so we change the index only when the key is pressed down.
         // holding down the button shouldn't change the index.
-        private Dictionary<Keys, Boolean> pressedKeys;
+        private Dictionary<Keys, int> pressedKeys;
         // the current selected Index. 
         private int selectedIndex;
 
+        private readonly int timeoutDelay = 60;
+
         public MainMenuSprite() : base(new Vector2())
         {
-            this.pressedKeys = new Dictionary<Keys, bool>();
+            this.pressedKeys = new Dictionary<Keys, int>();
             this.selectedIndex = 0;
         }
 
@@ -30,40 +32,56 @@ namespace Intro2DGame.Game.Sprites
             // Testing if the keys are in the dictionary to avoid any exceptions
             foreach (Keys k in ks.GetPressedKeys())
             {
-                if (!pressedKeys.ContainsKey(k)) pressedKeys.Add(k, false);
+                if (!pressedKeys.ContainsKey(k)) pressedKeys.Add(k, 0);
+            }
+
+            // decreasing the timeout by a frame
+            foreach (Keys k in pressedKeys.Keys)
+            {
+                if (pressedKeys[k] > 0) pressedKeys[k] = pressedKeys[k] - 1;
             }
 
             // Getting index down
-            if (ks.IsKeyDown(Keys.W) && !pressedKeys[Keys.W] || ks.IsKeyDown(Keys.Up) && !pressedKeys[Keys.Up])
+            if (ks.IsKeyDown(Keys.W) && pressedKeys[Keys.W] == 0 || ks.IsKeyDown(Keys.Up) && pressedKeys[Keys.Up] == 0)
             {
                 selectedIndex--;
-                pressedKeys[Keys.W] = true;
-                pressedKeys[Keys.Up] = true;
+                pressedKeys[Keys.W] = timeoutDelay;
+                pressedKeys[Keys.Up] = timeoutDelay;
             }
             else if (ks.IsKeyUp(Keys.W) && ks.IsKeyUp(Keys.Up))
             {
-                pressedKeys[Keys.W] = false;
-                pressedKeys[Keys.Up] = false;
+                pressedKeys[Keys.W] = 0;
+                pressedKeys[Keys.Up] = 0;
             }
 
             // Getting the index up
-            if (ks.IsKeyDown(Keys.S) && !pressedKeys[Keys.S] || ks.IsKeyDown(Keys.Down) && !pressedKeys[Keys.Down])
+            if (ks.IsKeyDown(Keys.S) && pressedKeys[Keys.S] == 0 || ks.IsKeyDown(Keys.Down) && pressedKeys[Keys.Down] == 0)
             {
                 selectedIndex++;
-                pressedKeys[Keys.S] = true;
-                pressedKeys[Keys.Down] = true;
+                pressedKeys[Keys.S] = timeoutDelay;
+                pressedKeys[Keys.Down] = timeoutDelay;
             }
             else if (ks.IsKeyUp(Keys.S) && ks.IsKeyUp(Keys.Down))
             {
-                pressedKeys[Keys.S] = false;
-                pressedKeys[Keys.Down] = false;
+                pressedKeys[Keys.S] = 0;
+                pressedKeys[Keys.Down] = 0;
             }
         }
 
         public override void Draw(SpriteBatch spriteBatch)
         {
             // TODO:
-            spriteBatch.DrawString(Game.FontArial, "Something! " + selectedIndex, new Vector2(100, 100), Color.Black);
+
+            spriteBatch.Draw(ImageManager.GetInstance().GetTexture2D("MenuItem/arrow"), new Vector2(50, 50 + selectedIndex * 50), Color.White);
+
+            spriteBatch.Draw(ImageManager.GetInstance().GetTexture2D("MenuItem/test"), new Vector2(100, 50), Color.White);
+            spriteBatch.Draw(ImageManager.GetInstance().GetTexture2D("MenuItem/test"), new Vector2(100, 100), Color.White);
+            spriteBatch.Draw(ImageManager.GetInstance().GetTexture2D("MenuItem/test"), new Vector2(100, 150), Color.White);
+            spriteBatch.Draw(ImageManager.GetInstance().GetTexture2D("MenuItem/test"), new Vector2(100, 200), Color.White);
+            spriteBatch.Draw(ImageManager.GetInstance().GetTexture2D("MenuItem/test"), new Vector2(100, 250), Color.White);
+
+
+            spriteBatch.DrawString(Game.FontArial, "Something! " + selectedIndex, new Vector2(100, 80), Color.Black);
         }
     }
 }
