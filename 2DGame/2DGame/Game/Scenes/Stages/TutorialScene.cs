4bab82d9f80361650c7e97744452348b7de83bef
GoLoCom <z221532@163.com>
2017-11-19 23:42:39 +0800

    2 Player Test

--- 2DGame/2DGame/Game/Scenes/Stages/TutorialScene.cs
+++ 2DGame/2DGame/Game/Scenes/Stages/TutorialScene.cs
@@ -1,5 +1,8 @@
 using Intro2DGame.Game.Sprites;
+using Intro2DGame.Game.Scenes;
+using Intro2DGame.Game.Sprites.Enemies.Orbs;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
 
 namespace Intro2DGame.Game.Scenes.Stages
 {
@@ -24,14 +27,33 @@ namespace Intro2DGame.Game.Scenes.Stages
     /// </summary>
 	internal class TutorialSprite : AbstractSprite
 	{
-
+        private int Shoot_Delay;
+        private int ShootDelay;
         public TutorialSprite(Vector2 position):base("tutorialplayer",position)
         {
-
+            ShootDelay = Shoot_Delay = 15;
         }
 		public override void Update(GameTime gameTime)
 		{
+            var MoveMent = new Vector2();
+            var Area = Game.GraphicsArea;
+            if (KeyboardManager.IsKeyPressed(Keys.Up)) MoveMent += new Vector2(0, -1);
+            if (KeyboardManager.IsKeyPressed(Keys.Down)) MoveMent += new Vector2(0, 1);
+            if (KeyboardManager.IsKeyPressed(Keys.Left)) MoveMent += new Vector2(-1, 0);
+            if (KeyboardManager.IsKeyPressed(Keys.Right)) MoveMent += new Vector2(1, 0);
+            MoveMent *= new Vector2(1.1f, 1.0f);
+            Position += MoveMent*4.25f;
+            // Prevents player from leaving the screen
+            if (Position.X + Texture.Width / 2f > Area.X) Position.X = Area.X - Texture.Width / 2f;
+            if (Position.Y + Texture.Height / 2f > Area.Y) Position.Y = Area.Y - Texture.Height / 2f;
+            if (Position.X - Texture.Width / 2f < 0) Position.X = Texture.Width / 2f;
+            if (Position.Y - Texture.Height / 2f < 100) Position.Y = 100 + Texture.Height / 2f;
 
-		}
+            if (ShootDelay--<=0 && KeyboardManager.IsKeyPressed(Keys.M))
+            {
+                ShootDelay = Shoot_Delay;
+                SpawnSprite(new LinearIncreasingOrb(Position, new Vector2(-1, 0), 0.235f,1.01025f));
+            }
+        }
 	}
 }
